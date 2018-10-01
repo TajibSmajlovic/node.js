2e07c63d8bec624031061f83cd050c3cf464841b
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-01 19:11:51 +0200

    04_web-servers-and-application-deployment: adds 02_creating-a-web-server, 03_rendering-templates-with-data and 04_advanced-templating

---
+++ 04_web-servers-and-application-deployment/02_creating-a-web-server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+var app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    // res.send('<h1> Hello Express <h2>')
+    res.send({
+        name: 'Tajib',
+        likes: [
+            'Biking',
+            'Cities'
+        ]
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.send('Hello Page')
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
