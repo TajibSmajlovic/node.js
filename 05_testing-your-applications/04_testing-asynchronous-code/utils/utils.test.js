37d858f0f7987ccbe51258a26e637c925005f72c
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-06 19:15:15 +0200

    05_testing-your-applications: adds 04_testing-asynchronous-code and 05_testing-express-applications

---
+++ 05_testing-your-applications/04_testing-asynchronous-code/utils/utils.test.js
@@ -0,0 +1,39 @@
+const expect = require('expect')
+
+const utils = require('./utils')
+
+it('should add two numbers',  () => {
+    var res = utils.add(33, 11)
+
+    expect(res).toBe(44).toBeA('number')
+});
+
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number')
+        done()
+    })
+});
+
+it('should square a number', function () {
+    var res = utils.square(5)
+
+    expect(res).toBe(25).toBeA('number')
+});
+
+it('should async square a number', (done) =>  {
+    utils.asyncSquare(5, (res) => {
+        expect(res).toBe(25).toBeA('number')
+        done()
+    })
+});
+
+it('should verify first and last names are set', () => {
+    var user = {location: 'Visoko', age:25}
+    var res = utils.setName(user, 'Tajib Smajlovic')
+
+    expect(res).toInclude({
+        firstName: 'Tajib',
+        lastName: 'Smajlovic'
+    })
+})
