3d96dedc300e2e596d257fc314f4efde853f4e05
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-14 15:07:35 +0200

    06_mongodb-mongoose-and-rest-api: adds 12_mongoose-queries-and-id-validation, 13_getting-an-individual-resource_get-todos-id and 14_testing-get-todos_id

---
+++ 06_mongodb-mongoose-and-rest-api/13_getting-an-individual-resource_get-todos-id/server/server.js
@@ -0,0 +1,54 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
+
+let {mongoose} = require('./db/mongoose')
+var {Todo} = require('./models/todo')
+var {User} = require('./models/user')
+
+let app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('ID not valid.')
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('ID not found')
+        }
+
+        res.send({todo})
+    }).catch((err) => res.status(400).send())
+})
+
+app.listen(3000, () => {
+    console.log('Started on port 3000')
+})
+
+module.exports = {app}
