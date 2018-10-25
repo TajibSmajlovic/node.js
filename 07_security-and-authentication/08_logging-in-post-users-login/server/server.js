f98b78c3c52a52b1013e99017b7ec59cfef85b04
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-25 21:26:48 +0200

    07_security-and-authentication: adds 07_testing-post-users-and-get-users-me and 08_logging-in-post-users-login

---
+++ 07_security-and-authentication/08_logging-in-post-users-login/server/server.js
@@ -0,0 +1,121 @@
+require('./config/config')
+
+const _ = require('lodash')
+const express = require('express')
+const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
+
+let {mongoose} = require('./db/mongoose')
+let {Todo} = require('./models/todo')
+let {User} = require('./models/user')
+let {authenticate} = require('./middleware/authenticate')
+
+let app = express()
+const port = process.env.PORT
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('ID not valid.')
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('ID not found')
+        }
+
+        res.send({todo})
+    }).catch((err) => res.status(400).send())
+})
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id
+
+    if (!ObjectID.isValid(id)) res.status(404).send('ID not valid.')
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+
+        todo ? res.status(200).send({todo}) : res.status(404).send()
+
+    }).catch((err) => res.status(400).send())
+})
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id
+    let body = _.pick(req.body, ['text', 'completed'])
+
+    if (!ObjectID.isValid(id)) res.status(404).send('ID not valid.')
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime()
+    } else {
+        body.completed = false
+        body.completedAt = null
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send
+        }
+
+        res.send({todo})
+    }).catch((e) => res.status(400).send())
+})
+
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password'])
+    let user = new User(body)
+
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth', token).send(user)
+    }).catch((e) => res.status(400).send(e))
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
+})
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password'])
+
+    User.findByCredential(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user)
+        })
+    }).catch((e) => {
+        res.status(400).send()
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`)
+})
+
+module.exports = {app}
